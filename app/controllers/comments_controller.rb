7b611fe171e13fc46ddf849c24195c7a55f2aa82
yuliyambg <yuliyambg@gmail.com>
2020-12-02 22:38:47 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
-  end
+      comment = Comment.create(comment_params)
+      redirect_to comment.post
+    end
 
-  private
+    private
 
-  def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
+    def comment_params
+      params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    end
 end
